e711ead707aafbdff50e8efc5ad1409a483d6d74
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2022-11-14 09:35:53 -0300

    exibição de página

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ const {User} = require("./User");
 
 const user = new User("Felipe");
 
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/html/index.html");
+});
+
+app.listen(8081, function(){
+    console.log("O app está rodando!");
+});
+
 console.log(user.saymyname());
+
